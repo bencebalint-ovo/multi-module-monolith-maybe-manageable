e5926c3d0d5fc3823a8a0ded5cb7c0dc0027a93d
Bence Balint <bence.balint@ovoenergy.com>
2023-05-29 12:48:46 +0100

    setup dependency

--- lambda1/lambda1/lambda_handler.py
+++ lambda1/lambda1/lambda_handler.py
@@ -1,2 +1,10 @@
+from lib1 import cats
+
 def lambda_handler(event):
-    print("hello! got this event %s" % event)
+    print('hello from lambda, got event "%s"' % event)
+    print("FYI %s" % cats.meow())
+
+
+if __name__ == "__main__":
+    print("running as main with empty event")
+    lambda_handler(None)
